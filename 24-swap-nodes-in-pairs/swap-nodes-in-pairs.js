c2c8780356ce08bb4a794ba573d36f26e8edc75a
mhzfahad6-maker <mhzfahad6@gmail.com>
2026-04-29 09:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (65.00%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    
+     let dummy = { next: head };
+    let prev = dummy;
+
+    while (head && head.next) {
+        let first = head;
+        let second = head.next;
+        prev.next = second;
+        first.next = second.next;
+        second.next = first;
+        prev = first;
+        head = first.next;
+    }
+
+    return dummy.next;
+};
